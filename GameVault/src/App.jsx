f95252c39ec5ed86cbd1be274f7e91b6cef728e9
Andres Heredia <andres.h.aguilar@gmail.com>
2025-03-17 19:33:33 -0600

    CreaTuLanding1_AndresHeredia

--- GameVault/src/App.jsx
+++ GameVault/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"; 
+import Footer from "./components/Footer";
 import ItemListContainer from "./components/ItemListContainer";
 
 const App = () => {
@@ -11,14 +11,29 @@ const App = () => {
         <Navbar />
         <main className="content">
           <Routes>
-            <Route path="/" element={<ItemListContainer message="¡Bienvenido a GameVault!" />} />
-            <Route path="/monitores" element={<ItemListContainer message="Explora nuestros monitores gamer" />} />
-            <Route path="/consolas" element={<ItemListContainer message="Encuentra las mejores consolas" />} />
-            <Route path="/computadoras" element={<ItemListContainer message="PCs de alto rendimiento" />} />
-            <Route path="/accesorios" element={<ItemListContainer message="Accesorios para mejorar tu setup" />} />
+            <Route 
+              path="/" 
+              element={<ItemListContainer message="¡Bienvenido a GameVault!" />} 
+            />
+            <Route 
+              path="/monitores" 
+              element={<ItemListContainer message="Explora nuestros monitores gamer" />} 
+            />
+            <Route 
+              path="/consolas" 
+              element={<ItemListContainer message="Encuentra las mejores consolas" />} 
+            />
+            <Route 
+              path="/computadoras" 
+              element={<ItemListContainer message="PCs de alto rendimiento" />} 
+            />
+            <Route 
+              path="/accesorios" 
+              element={<ItemListContainer message="Accesorios para mejorar tu setup" />} 
+            />
           </Routes>
         </main>
-        <Footer /> 
+        <Footer />
       </div>
     </Router>
   );
